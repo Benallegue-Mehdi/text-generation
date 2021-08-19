4904c47cbbac55735fa3b98e10c5385b50c823d6
toxicscorpius <toxicscorpius@gmail.com>
2021-08-19 16:01:28 -0400

    More efficient conditions + training program becomes a function

--- probability_table.py
+++ probability_table.py
@@ -1,23 +1,29 @@
-text = ''
-
-with open('training_text.txt', 'r') as file:
-    for line in file:
-        text += line.replace('\n', ' ').replace(' !', '!').replace(' ?', '?').replace(' :', ':').replace(' ;', ';').replace('« ', '').replace(' »', '')
-
 END = object()
-words = [END]
-words += text.split()
 
-end_punctuation = ['.', '!', '?']
-for pos, word in enumerate(words):
-    if word != END:
-        if word[-1] in end_punctuation:
-            words.insert(pos + 1, END)
+def probability_table(path_to_file: str='Bible_Crampon_1923_Genèse.txt') -> dict:
+    text = ''
 
-probability_table = {}
-for index, word in enumerate(words):
-    if index < len(words) - 1:
-        if word in probability_table:
-            probability_table[word].append(words[index + 1])
-        else:
-            probability_table[word] = [words[index + 1]]
+    with open(path_to_file, 'r') as file:
+        for line in file:
+            text += line.replace('\n', ' ').replace(' !', '!').replace(' ?', 
+            '?').replace(' :', ':').replace(' ;', ';').replace('« ', 
+            '').replace(' »', '')
+
+    words = [END]
+    words += text.split()
+
+    end_punctuation = ['.', '!', '?']
+    for pos, word in enumerate(words):
+        if word != END:
+            if word[-1] in end_punctuation:
+                words.insert(pos + 1, END)
+
+    probability_table = {}
+    previous, *words = words
+    for word in words:
+        try:
+            probability_table[previous].append(word)
+        except:
+            probability_table[previous] = [word]
+        previous = word
+    return probability_table
--- sentence_generator.py
+++ sentence_generator.py
@@ -1,11 +1,12 @@
 import random
 from probability_table import probability_table, END
 
-current_word = random.choice( probability_table[END])
+table = probability_table('Bible_Crampon_1923_Genèse.txt')
+current_word = random.choice(table[END])
 result = current_word
 
 while True:
-    current_word = random.choice( probability_table[current_word])
+    current_word = random.choice(table[current_word])
     if current_word != END:
         result += f' {current_word}'
     else:
